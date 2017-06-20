52b5308e88c1c7350127c06d3050bcb496817cd4
Andrej <root@home>
2017-06-20 20:06:31 +0300

    Update checkpoint.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -34,8 +34,8 @@ namespace Checkpoints {
 
         MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
         if (i == checkpoints.end()) return true;
-        //return hash == i->second;
-        return true;
+        return hash == i->second;
+        //return true;
     }
 
     //! Guess how far we are in the verification process at the given block index
@@ -77,8 +77,8 @@ namespace Checkpoints {
 
         const MapCheckpoints& checkpoints = *Params().Checkpoints().mapCheckpoints;
 
-        //return checkpoints.rbegin()->first;
-        return 0;
+        return checkpoints.rbegin()->first;
+        //return 0;
     }
 
     CBlockIndex* GetLastCheckpoint()
@@ -93,8 +93,8 @@ namespace Checkpoints {
             const uint256& hash = i.second;
             BlockMap::const_iterator t = mapBlockIndex.find(hash);
             if (t != mapBlockIndex.end())
-                //return t->second;
-                return NULL;
+                return t->second;
+                //return NULL;
 
         }
         return NULL;
