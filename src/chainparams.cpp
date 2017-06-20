fcc6f1ac7f373b10f661784da4b768b1e7e2ad9c
Andrej <root@home>
2017-06-20 11:45:31 +0300

    Checpiont

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -49,7 +49,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 static const Checkpoints::CCheckpointData data = {
         &mapCheckpoints,
         1497684047, // * UNIX timestamp of last checkpoint block
-        272,          // * total number of transactions between genesis and last checkpoint
+        271,          // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         500.0     // * estimated number of transactions per day after checkpoint
     };
